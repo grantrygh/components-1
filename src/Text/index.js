c6d437e21d4efc0a314b58c9712425920cbe51bb
Grant Rygh <grantrygh@gmail.com>
2020-03-10 15:08:04 -0700

    text color change on theme

--- src/Text/index.js
+++ src/Text/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Box from '../Box';
+import { useColorMode } from '../ColorModeProvider';
+import { useTheme } from '../ThemeProvider';
 
 const Text = React.forwardRef((props, ref) => {
-    return <Box ref={ref} as="p" fontFamily="body" {...props} />;
+    const { colorMode } = useColorMode();
+    const { colors } = useTheme();
+    const color = { light: colors.black, dark: colors.white };
+
+    return <Box ref={ref} as="p" fontFamily="body" color={color[colorMode]} {...props} />;
 });
 
 Text.displayName = 'Text';

81341b3b086835c9598bfe1dc3b1e72f3ba9a809
Grant Rygh <grantrygh@gmail.com>
2020-03-10 15:07:44 -0700

    Ref Audentio/components#9

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,9 @@
 import { addDecorator, configure } from '@storybook/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { CSSReset, ThemeProvider } from '../src';
+import Box from '../src/Box';
+import Button from '../src/Button';
+import { DarkMode, LightMode } from '../src/ColorModeProvider';
 
 const req = require.context('../src', true, /examples\.(js|mdx)$/);
 
@@ -8,11 +11,46 @@ function loadStories() {
     req.keys().forEach(filename => req(filename));
 }
 
+const StorybookBg = {
+    light: 'white',
+    dark: 'gray.800',
+};
+
 const AppProvider = ({ children }) => {
+    const [CurrentColorMode, setCurrentColorMode] = useState(() => LightMode);
+    const color = CurrentColorMode().props.value;
+
     return (
         <ThemeProvider>
             <CSSReset />
-            {children}
+            <CurrentColorMode>
+                <Box position="fixed" right={4} top={4} zIndex={1}>
+                    <Button
+                        onClick={() => {
+                            setCurrentColorMode(() => LightMode);
+                        }}
+                        size="sm"
+                        bg="white"
+                        color="black"
+                    >
+                        Light
+                    </Button>
+
+                    <Button
+                        onClick={() => {
+                            setCurrentColorMode(() => DarkMode);
+                        }}
+                        size="sm"
+                        bg="black"
+                        color="white"
+                    >
+                        Dark
+                    </Button>
+                </Box>
+                <Box bg={StorybookBg[color]} minHeight="100vh">
+                    {children}
+                </Box>
+            </CurrentColorMode>
         </ThemeProvider>
     );
 };

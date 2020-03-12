399e9c7cff3b8c34f9f71103c1bb754da34abc87
Grant Rygh <grantrygh@gmail.com>
2020-03-12 10:51:34 -0700

    move to components

--- src/Lightbox/index.js
+++ src/Lightbox/index.js
@@ -1,98 +1,14 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { createContext, useContext, useEffect, useState } from 'react';
 import Box from '../Box';
 import { useColorMode } from '../ColorModeProvider';
-import Flex from '../Flex';
-import Image from '../Image';
 import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from '../Modal';
 import { SlideIn } from '../Transition';
-
-const GalleryContext = createContext();
-
-// context provider hoc
-const LightboxGalleryProvider = props => {
-    const [activeItem, setActiveItem] = useState(null);
-    const [mediaList, setMediaList] = useState([]);
-    const media = [];
-
-    const register = mediaItem => {
-        media.push(mediaItem);
-        setMediaList(media);
-    };
-    const unregister = mediaItem => {
-        setMediaList(mediaList.filter(item => item !== mediaItem));
-    };
-
-    const context = { register, unregister, media: mediaList, activeItem, setActiveItem };
-
-    return (
-        <GalleryContext.Provider value={context}>
-            {props.children}
-            <LightboxGallery isOpen={!activeItem} />
-        </GalleryContext.Provider>
-    );
-};
-
-const useGalleryContext = () => {
-    const context = useContext(GalleryContext);
-    if (context === undefined) {
-        throw new Error('useGalleryContext must be used within a LightboxGalleryProvider');
-    }
-    return context;
-};
-
-const LightboxMedia = ({ src, skip, children }) => {
-    const context = useGalleryContext();
-
-    useEffect(() => {
-        if (!skip) {
-            // add image to lightbox context media array on mount
-            context.register(src);
-
-            // remove image from lightbox context media array on unmount
-            return () => {
-                context.unregister(src);
-            };
-        }
-    }, [src]);
-
-    // when the media item is clicked, set the context's active item to the media src
-    return (
-        <Box
-            onClick={() => {
-                if (!skip) {
-                    context.setActiveItem(src);
-                }
-            }}
-            cursor={!skip && 'pointer'}
-        >
-            {children}
-        </Box>
-    );
-};
-
-// Lightbox with image gallery as content
-const LightboxGallery = () => {
-    const context = useGalleryContext();
-    return (
-        <Lightbox isOpen={!!context.activeItem} onClose={() => context.setActiveItem(null)}>
-            {/* // TODO: this will be replaced by (carousel?) media navigation. context.activeItem can be used to determine starting slide in the carousel */}
-            <Box>replace with carousel</Box>
-            <Flex justify="center">
-                {context.media.map(mediaItem => (
-                    <Box>
-                        <Image src={mediaItem} />
-                    </Box>
-                ))}
-            </Flex>
-        </Lightbox>
-    );
-};
+import { LightboxGalleryControls, LightboxGalleryProvider, LightboxMedia, useGalleryContext } from './components';
 
 const Lightbox = props => {
     const { colorMode } = useColorMode();
-    const { isOpen, onClose, children } = props;
+    const { isOpen, onClose, showControls, children } = props;
     const bg = { light: 'white', dark: 'black' };
 
     return (
@@ -103,7 +19,8 @@ const Lightbox = props => {
                         <ModalOverlay opacity={1} bg={bg[colorMode]}>
                             <ModalCloseButton />
                         </ModalOverlay>
-                        <ModalContent {...styles} shadow={0} bg="transparent">
+                        {showControls && <LightboxGalleryControls />}
+                        <ModalContent {...styles} shadow={0} bg="transparent" h="100%" my={0} py={4}>
                             {children}
                         </ModalContent>
                     </Modal>

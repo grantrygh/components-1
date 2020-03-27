ad4438d750c134870187fe264b1d44ff1e994daa
Grant Rygh <grantrygh@gmail.com>
2020-03-27 15:14:46 -0700

    Textarea refactor. Ref Audentio/components#41

--- src/Textarea/examples.tsx
+++ src/Textarea/examples.tsx
@@ -1,10 +1,14 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import { storiesOf } from '@storybook/react';
-import Textarea from '.';
+import Textarea, { ExpandingTextarea } from '.';
 
 const stories = storiesOf('Textarea', module);
 
 stories.add('Default', () => (
     <Textarea maxWidth="sm" mx="auto" mt={2} placeholder="Here is a sample placeholder" size="md" />
+));
+
+stories.add('Expanding', () => (
+    <ExpandingTextarea maxWidth="sm" mx="auto" placeholder="Here is a sample placeholder" size="md" />
 ));

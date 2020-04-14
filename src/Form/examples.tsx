dadbe96621f606c5ffc6a2932d4c7bfc639cdc80
Grant Rygh <grantrygh@gmail.com>
2020-04-14 15:28:25 -0700

    update form  sample storybook example

--- src/Form/examples.tsx
+++ src/Form/examples.tsx
@@ -14,7 +14,6 @@ import {
     Radio,
     RadioGroup,
     Select,
-    Stack,
     Switch,
     Text,
 } from '..';
@@ -34,32 +33,33 @@ stories.add('Sample', () => {
                 initialValue={{
                     first_name: 'testname',
                     gender: 'male',
-                    age: '4',
+                    age: 20,
                     chocolate: true,
                     caramel: false,
                     flavor: 'mango',
                     lactose_intolerant: true,
                 }}
             >
-                <InputGroup label="First Name">
-                    <Input name="first_name" />
+                {/* <Input> should be wrapped by an <InputGroup> for form controls, label, and support for InputLeftElement, etc (see InputGroup) */}
+                <InputGroup label="First Name" name="first_name">
+                    <Input />
                 </InputGroup>
 
-                <InputGroup label="Age">
-                    <NumberInput max={35} min={0} step={1} name="age" />
+                {/* Same as for <Input> */}
+                <InputGroup label="Age" name="age">
+                    <NumberInput max={100} min={0} step={1} />
                 </InputGroup>
 
-                <RadioGroup name="gender">
-                    <Radio value="male" name="male">
-                        Male
-                    </Radio>
-                    <Radio value="female" name="female">
-                        Female
-                    </Radio>
+                {/* Since radio groups should have just one value selected, "name" is placed as a <RadioGroup> prop */}
+                <RadioGroup label="Gender" name="gender">
+                    <Radio value="male">Male</Radio>
+                    <Radio value="female">Female</Radio>
                 </RadioGroup>
-                <InputGroup label="Ice cream flavor">
+
+                {/* <Select> should be wrapped in an <InputGroup> component to apply form controls and label */}
+                <InputGroup label="Ice cream flavor" name="flavor">
                     <Select
-                        name="flavor"
+                        isMulti
                         options={[
                             { value: 'chocolate', label: 'Chocolate' },
                             { value: 'vanilla', label: 'Vanilla' },
@@ -79,25 +79,31 @@ stories.add('Sample', () => {
                         ]}
                     />
                 </InputGroup>
-                <CheckboxGroup name="checkboxGroup">
+
+                {/* <CheckboxGroup> wrapped for form control and label. "name" should be passed to individual checkboxes, as multiple can be selected. */}
+                <CheckboxGroup name="checkboxGroup" label="Toppings">
                     <Checkbox name="chocolate">Chocolate</Checkbox>
                     <Checkbox name="caramel">Caramel</Checkbox>
                 </CheckboxGroup>
 
-                <Stack>
-                    <Switch name="lactose_intolerant">I'm lactose intolerant</Switch>
-                </Stack>
+                {/* <Switch> components can just be wrapped in a <Stack> component which will apply consistent spacing as other InputGroups */}
+                <InputGroup name="lactose_intolerant">
+                    <Switch>I'm lactose intolerant</Switch>
+                </InputGroup>
 
                 <Button type="submit">Submit</Button>
             </Form>
+
             {formValue && (
                 <Box>
                     <Heading kind="h4" />
-                    {Object.keys(formValue).map(key => (
-                        <Text>
-                            {key} : {formValue[key].toString()}
-                        </Text>
-                    ))}
+                    {Object.keys(formValue).map(key => {
+                        return (
+                            <Text>
+                                {key} : {formValue[key].toString()}
+                            </Text>
+                        );
+                    })}
                 </Box>
             )}
         </Box>

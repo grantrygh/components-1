b0ac07288c78048411923bd20b130815be0d2939
Grant Rygh <grantrygh@gmail.com>
2020-04-21 15:46:09 -0700

    styleguide header update

---
+++ examples/styleguide/components/Header/styles.tsx
@@ -0,0 +1,49 @@
+import { useTheme } from '../../../../src/ThemeProvider';
+
+export const HeaderStyle = (props, theme) => ({
+    style: {
+        //
+    },
+    kinds: {
+        primary: {
+            icon: {
+                mr: 4,
+                color: 'faintText',
+                cursor: 'pointer',
+            },
+        },
+        secondary: {
+            root: {
+                display: { _: 'none', lg: 'flex' },
+            },
+            icon: {
+                name: 'search',
+                color: 'faintText',
+            },
+            input: {
+                placeholder: 'Search...',
+            },
+        },
+        tertiary: {
+            icon: {
+                mr: 4,
+                color: 'faintText',
+                cursor: 'pointer',
+            },
+        },
+    },
+});
+
+const useHeaderStyle = props => {
+    const theme = useTheme();
+    const styles = theme['styles'].Header ? theme['styles'].Header(props, theme) : HeaderStyle(props, theme);
+
+    return {
+        // base style
+        root: styles.style,
+
+        headerSection: styles.kinds[props.kind],
+    };
+};
+
+export default useHeaderStyle;

5a62bc16726f60a627d7bac0096890a9c1f439fb
Grant Rygh <grantrygh@gmail.com>
2020-05-28 15:49:54 -0700

    fix error from React not being in scope

--- src/Spinner/examples.tsx
+++ src/Spinner/examples.tsx
@@ -1,16 +1,17 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import { storiesOf } from '@storybook/react';
+import React from 'react';
 import { Spinner } from '.';
 import { ISpinner } from './types';
 
 const stories = storiesOf('Spinner', module);
 stories.add('Default', () => (
-    <>
+    <React.Fragment>
         {['xl', 'lg', 'md', 'sm', 'xs'].map((size: ISpinner['size']) => (
             <Spinner m={3} size={size} />
         ))}
-    </>
+    </React.Fragment>
 ));
 
 stories.add('Color', () => (

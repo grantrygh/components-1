6bd3571c4daf1c00ddba6d94e31a72991b082275
Grant Rygh <grantrygh@gmail.com>
2020-06-12 17:12:37 -0700

    heading using wrong "as"

--- src/Heading/Heading.tsx
+++ src/Heading/Heading.tsx
@@ -1,11 +1,11 @@
-import React, { forwardRef } from 'react';
+import React from 'react';
 import { Box } from '../Box';
 import useHeadingStyle from './styles';
 import { HeadingProps } from './types';
 
-export const Heading = forwardRef((props: HeadingProps, ref) => {
-    const { kind = 'h1', ...rest } = props;
+export const Heading = (props: HeadingProps, ref) => {
+    const { kind = 'h4', ...rest } = props;
     const headingStyle = useHeadingStyle({ kind });
 
-    return <Box ref={ref} as="h2" {...headingStyle} {...rest} />;
-});
+    return <Box as={kind} {...headingStyle} {...rest} />;
+};

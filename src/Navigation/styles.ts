f836cadc898daa8cf4418a9bb0c26e605c633da3
Grant Rygh <grantrygh@gmail.com>
2020-06-30 13:49:57 -0700

    misc style fixes for components

--- src/Navigation/styles.ts
+++ src/Navigation/styles.ts
@@ -25,7 +25,8 @@ export const navigationStyle: componentStyleDef<NavigationProps & NavigationItem
     return {
         style,
         navItem: {
-            align: 'center',
+            display: 'flex',
+            alignItems: 'center',
             flex: 1,
             minHeight: 50,
             mb: !isSubmenuItem && 3,

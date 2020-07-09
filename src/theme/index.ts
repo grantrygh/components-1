35d072c6e5423e1b0c835cc765327432b360267f
Kyler <kylerc@audentio.com>
2020-07-09 13:40:45 -0500

    Add / update indexes with missing files

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,2 +1,7 @@
+export * from './colors';
 export * from './colors-utils';
+export * from './sizes';
 export * from './theme';
+export * from './theme-utils';
+export * from './types';
+export * from './typography';
---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+export * from './assignRef';
+export * from './createChainedFunction';
+export * from './getFocusables';
+export * from './isObject';
+export * from './isReducedMotion';
+export * from './prefersReducedMotion';
+export * from './setRef';
+// export * from './utils';
+export * from './wrapEvent';

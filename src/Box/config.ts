f2d7b247e4c1e1911178e7d7a8f6dd8f7cc46818
Grant Rygh <grantrygh@gmail.com>
2020-10-23 17:59:50 -0700

    support columnCount on <Box>

--- src/Box/config.ts
+++ src/Box/config.ts
@@ -115,6 +115,7 @@ export const config = {
     listStyleType: true,
     listStylePosition: true,
     listStyleImage: true,
+    columnCount: true,
     fill: {
         property: 'fill',
         scale: 'colors',

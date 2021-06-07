9b7357e26510f3ffd8cde6edb0b3cb882d31fce7
Grant Rygh <grantrygh@gmail.com>
2021-06-07 15:26:43 -0700

    revert to context, next value undefined

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,23 +1,31 @@
+import * as H from 'history';
 import { useRouter as useNextRouter } from 'next/router';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useContext } from 'react';
+import { RouteComponentProps, StaticContext, __RouterContext as RouterContext } from 'react-router';
 import { isNextApp } from '../utils/isNextApp';
 
+export function useReactRouter<
+    Params extends { [K in keyof Params]?: string } = {},
+    C extends StaticContext = StaticContext,
+    S = H.LocationState
+>() {
+    return useContext(RouterContext) as RouteComponentProps<Params, C, S>;
+}
+
 export const useRouter = () => {
-    const location = useLocation();
-    const history = useHistory();
+    // const location = useLocation();
+    // const history = useHistory();
+    const { location, history } = useReactRouter();
 
     const router = useNextRouter();
-
-    console.log('in this test', isNextApp);
-
     // next
-    // if (isNextApp()) {
-    return {
-        pathname: router?.pathname,
-        search: router?.query,
-        router,
-    };
-    // }
+    if (isNextApp()) {
+        return {
+            pathname: router?.pathname,
+            search: router?.query,
+            router,
+        };
+    }
 
     // react-router
     return {

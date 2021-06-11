e58113c18cb7bfb9b38cb2298899d14f6bc793ec
Ian-Hitt <ianh@audentio.com>
2021-06-11 08:24:28 -0500

    revert input default size

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -7,7 +7,7 @@ import { InputProps } from './types';
 
 export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
     const {
-        size = 'lg',
+        size = 'md',
         variant = 'filled',
         as = 'input',
         'aria-label': ariaLabel,

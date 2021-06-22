0d67f2e1362327afe5e8ce7031809f632e96e55a
Yousif Faraj <yfaraj96@gmail.com>
2021-06-22 11:17:31 -0400

    Changed old storybook name and added a new storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-actions/register',
+        '@storybook/addon-storysource/register',
+        '@storybook/addon-viewport/register',
+    ],
+};

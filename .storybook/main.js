d37aba8250575f32c75a72dc4d795b24e2b08950
Yousif Faraj <yfaraj96@gmail.com>
2021-06-23 11:13:43 -0400

    refactored webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -61,18 +61,12 @@ module.exports = {
     },
     webpackFinal: (config) => {
         const { css, scss, js } = loaders;
-
-        config.resolve.modules.push(PATHS.base);
-
-        // config.module.rules.push(css({ __DEV__: true, useStyleLoader: true, __BROWSER__: true }));
-        config.module.rules.push(scss({ __DEV__: true, useStyleLoader: true, __BROWSER__: true }));
-
-        config.module.rules.push(js);
-        config.module.rules = config.module.rules.concat(file_loaders);
-
-        // Get pass an emotion version issue without downgrading
         return merge(config, {
+            module: {
+                rules: [scss({ __DEV__: true, useStyleLoader: true, __BROWSER__: true }), js, ...file_loaders],
+            },
             resolve: {
+                modules: [PATHS.base],
                 alias: {
                     '@emotion/core': getPackageDir('@emotion/react'),
                     '@emotion/styled': getPackageDir('@emotion/styled'),

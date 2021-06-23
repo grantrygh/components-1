f3b460c4cf1c2211f724e7cf85b2400e619472d2
Yousif Faraj <yfaraj96@gmail.com>
2021-06-23 08:52:21 -0400

    Storybook up and running/webpack update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 const { loaders } = require('../webpack/common');
+const path = require('path');
+const fs = require('fs');
 const PATHS = require('../webpack/paths');
+const { merge } = require('webpack-merge');
 
 const file_loaders = [
     // Images & Videos
@@ -22,8 +25,22 @@ const file_loaders = [
     },
 ];
 
+const getPackageDir = (filepath) => {
+    let currDir = path.dirname(require.resolve(filepath));
+    while (true) {
+        if (fs.existsSync(path.join(currDir, 'package.json'))) {
+            return currDir;
+        }
+        const { dir, root } = path.parse(currDir);
+        if (dir === root) {
+            throw new Error(`Could not find package.json in the parent directories starting from ${filepath}.`);
+        }
+        currDir = dir;
+    }
+};
+
 module.exports = {
-    stories: ['../src/**/stories.tsx'],
+    stories: ['../src/**/examples.tsx'],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
@@ -35,7 +52,11 @@ module.exports = {
         return {
             ...options,
             presets: [...options.presets, require.resolve('next/babel')],
-            plugins: [...options.plugins, require.resolve('@emotion/babel-plugin')],
+            plugins: [
+                ...options.plugins,
+                require.resolve('@emotion/babel-plugin'),
+                [require.resolve('@babel/plugin-proposal-private-property-in-object'), { loose: true }],
+            ],
         };
     },
     webpackFinal: (config) => {
@@ -59,6 +80,15 @@ module.exports = {
         config.module.rules.push(js);
         config.module.rules = config.module.rules.concat(file_loaders);
 
-        return { ...config };
+        // Get pass an emotion version issue without downgrading
+        return merge(config, {
+            resolve: {
+                alias: {
+                    '@emotion/core': getPackageDir('@emotion/react'),
+                    '@emotion/styled': getPackageDir('@emotion/styled'),
+                    'emotion-theming': getPackageDir('@emotion/react'),
+                },
+            },
+        });
     },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,56 @@
-export const parameters = {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
+import { default as React, useState } from 'react';
+import { Box } from '../src/Box';
+import { Button } from '../src/Button';
+import { CSSReset } from '../src/CSSReset';
+import { ThemeProvider } from '../src/ThemeProvider';
+
+const storyStyle = {
+    light: {
+        pageBg: 'white',
+        switchTo: {
+            label: 'Dark',
+            value: 'dark',
         },
+        buttonBg: 'black',
+        buttonColor: 'white',
+    },
+    dark: {
+        pageBg: 'gray.800',
+        switchTo: {
+            label: 'Light',
+            value: 'light',
+        },
+        buttonBg: 'white',
+        buttonColor: 'black',
     },
 };
+
+const AppProvider = ({ children }) => {
+    const [currentColorMode, setCurrentColorMode] = useState('light');
+    const story = storyStyle[currentColorMode];
+
+    return (
+        <ThemeProvider>
+            <CSSReset />
+            {/* <CurrentColorMode> */}
+            <Box position="fixed" right={4} top={4} zIndex={1}>
+                <Button
+                    onClick={() => {
+                        setCurrentColorMode(story.switchTo.value);
+                    }}
+                    size="sm"
+                    bg={story.buttonBg}
+                    color={story.buttonColor}
+                >
+                    Switch to {story.switchTo.label} mode
+                </Button>
+            </Box>
+            <Box bg={story.pageBg} minHeight="100vh">
+                {children}
+            </Box>
+            {/* </CurrentColorMode> */}
+        </ThemeProvider>
+    );
+};
+
+export const decorators = [(Story) => <AppProvider>{Story()}</AppProvider>];

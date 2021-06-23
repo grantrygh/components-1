cc7f0d3306492266cb5810fc726129d7f2ce040b
Tushar Singh <tusharsingh@gmx.com>
2021-06-24 00:37:09 +0530

    use logo in storybook

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,5 +3,6 @@ import { create } from '@storybook/theming';
 export default create({
     base: 'light',
     brandTitle: '@audentio/stuff',
-    brandUrl: 'https://audent.io',
+    brandUrl: 'https://github.com/Audentio/components',
+    brandImage: 'https://cdn.statically.io/gh/Audentio/components/1.0/logo/logo-dark.svg',
 });

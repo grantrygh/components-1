292994db3dd221de2ac68a818dad05dc174b355e
Grant Rygh <grantrygh@gmail.com>
2021-06-23 18:01:31 -0700

    refactor: update getFormattedOffset return

--- src/Timestamp/getFormattedOffset.ts
+++ src/Timestamp/getFormattedOffset.ts
@@ -5,7 +5,9 @@ export function getFormattedOffset() {
         const hours = Math.floor(-d.getTimezoneOffset() / 60);
         const minutes = -d.getTimezoneOffset() % 60;
 
-        formattedOffset = `${hours < 0 ? '-' : '+'}${hours}:${minutes}`;
+        formattedOffset = `${hours < 0 ? '-' : '+'}${Math.abs(hours).toString().padStart(2, '0')}:${minutes
+            .toString()
+            .padStart(2, '0')}`;
     }
 
     return formattedOffset;
